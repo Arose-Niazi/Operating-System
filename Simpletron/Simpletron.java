a2768a3605b2e94b176639e8068e94a7e71eaa09
Arose Niazi <arose.niazi@yahoo.com>
2020-09-24 12:50:12 +0500

    Inital Commits

---
+++ Simpletron/Simpletron.java
@@ -0,0 +1,20 @@
+/*
+    Simpletron system developed by @Arose-Niazi
+    Started on: 24/9/2020
+ */
+
+public class Simpletron {
+    /*
+        Input output operations constants.
+    */
+    private static final int READ = 10;
+    private static final int WRITE = 11;
+
+    /*
+        Load store operations constants.
+    */
+    private static final int LOAD = 20;
+    private static final int STORE = 21;
+
+
+}
