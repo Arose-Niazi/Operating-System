8fd7295e2d4deaa039e36b761d32661d98fc5291
Arose Niazi <arose.niazi@yahoo.com>
2020-09-24 12:59:54 +0500

    Added Constatnts

---
+++ Simpletron Simulation/src/Simpletron.java
@@ -0,0 +1,35 @@
+/*
+    Simpletron system developed by @Arose-Niazi
+    Started on: 24/9/2020
+ */
+
+public class Simpletron {
+    /*
+        Input output operations constants.
+    */
+    private static final int READ = 10;
+    private static final int WRITE = 11;
+
+    /*
+        Load store operations constants.
+    */
+    private static final int LOAD = 20;
+    private static final int STORE = 21;
+
+    /*
+        Calculation operations constants.
+    */
+    private static final int ADD = 30;
+    private static final int SUB = 31;
+    private static final int DIV = 32;
+    private static final int MUL = 33;
+
+    /*
+        Control operations constants.
+    */
+    private static final int BRANCH = 40;
+    private static final int BRANCHN = 41;
+    private static final int BRANCHZ = 42;
+    private static final int HALT = 43;
+
+}
