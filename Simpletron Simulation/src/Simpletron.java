9afc3c5139784c295166d9f11a3f330fcddef2a5
Arose Niazi <arose.niazi@yahoo.com>
2020-09-24 14:43:41 +0500

    Completed

--- Simpletron Simulation/src/Simpletron.java
+++ Simpletron Simulation/src/Simpletron.java
@@ -3,6 +3,8 @@
     Started on: 24/9/2020
  */
 
+import javax.xml.soap.SOAPPart;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Simpletron {
@@ -39,6 +41,7 @@ public class Simpletron {
      */
     private static final int END = -9999;
     private static final char IDENTIFIER = '?';
+    private static final int SIZE = 46;
 
     /*
         Variables
@@ -51,35 +54,120 @@ public class Simpletron {
     /*
         Working
      */
-    public Simpletron()
-    {
-        memory = new int[100];
-        accumulator= instructionCounter= instructionRegister= 0;
+    public Simpletron() {
+        memory = new int[SIZE];
+        accumulator = instructionCounter = instructionRegister = 0;
         boot();
         run();
     }
 
-    private void boot()
-    {
+    private void boot() {
         System.out.println("*** Welcome to Simpletron! ***\n" +
                 "*** Please enter your program one instruction ***\n" +
                 "*** (or data word) at a time. I will display ***\n" +
-                "*** the location number and a question mark ("+ IDENTIFIER +"). ***\n" +
+                "*** the location number and a question mark (" + IDENTIFIER + "). ***\n" +
                 "*** You then type the word for that location. ***\n" +
-                "*** Type "+ END +" to stop entering your program. ***");
+                "*** Type " + END + " to stop entering your program. ***");
     }
 
-    private void run()
-    {
-        int input=0;
-        Scanner scanner = new Scanner(System.in);
-        for(int counter=0; counter<memory.length && input != END; counter++)
-        {
-            System.out.printf("%02d %c ",counter,IDENTIFIER);
-            input = scanner.nextInt();
-        }
+    private void run() {
+        getInstructions();
+
         System.out.println("*** Program loading completed ***\n" +
                 "*** Program execution begins ***");
+
+        compute();
+        memoryDump();
+    }
+
+    private void getInstructions() {
+        int input = 0;
+        Scanner scanner = new Scanner(System.in);
+        for (int counter = 0; counter < SIZE && input != END; counter++) {
+            System.out.printf("%02d %c ", counter, IDENTIFIER);
+            input = scanner.nextInt();
+            if(input > 9999 || input < -9999) {
+                counter--;
+                continue;
+            }
+            if(input != END)
+                memory[counter] = input;
+        }
+    }
+
+    private void compute() {
+        if(instructionCounter >= memory.length) return;
+        instructionRegister = memory[instructionCounter];
+        if(instructionRegister == 0) return;
+        final int operand = instructionRegister % SIZE;
+
+        switch (instructionRegister / SIZE)
+        {
+            case READ:
+                System.out.println("Enter an integer.\n ? ");
+                Scanner scanner = new Scanner(System.in);
+                int input = scanner.nextInt();
+                memory[operand] = input;
+                break;
+            case WRITE:
+                System.out.printf("Memory Location %02d: %+d %n",operand,memory[operand]);
+                break;
+
+            case LOAD:
+                accumulator = memory[operand];
+                break;
+            case STORE:
+                memory[operand] = accumulator;
+                break;
+
+            case ADD:
+                accumulator += memory[operand];
+                break;
+            case SUB:
+                accumulator -= memory[operand];
+                break;
+            case DIV:
+                accumulator /= memory[operand];
+                break;
+            case MUL:
+                accumulator *= memory[operand];
+                break;
+
+            case BRANCH:
+                instructionCounter = operand-1;
+                break;
+            case BRANCHN:
+                if(accumulator < 0) instructionCounter = operand-1;
+                break;
+            case BRANCHZ:
+                if(accumulator == 0) instructionCounter = operand-1;
+                break;
+            case HALT:
+                System.out.println("*** Simpletron execution terminated ***");
+                return;
+        }
+        instructionCounter++;
+        compute();
+    }
+
+    private void memoryDump()
+    {
+        System.out.println("REGISTERS:");
+        System.out.printf("%-20s %+05d%n","accumulator",accumulator);
+        System.out.printf("%-22s  %02d%n","instructionCounter",instructionCounter);
+        System.out.printf("%-20s %+05d%n%n","instructionRegister",instructionRegister);
+
+        System.out.println("MEMORY:");
+        System.out.print("   ");
+        for(int i=0; i< ((SIZE+10) % SIZE); i++)
+            System.out.printf("    %02d ",i);
+        for(int i=0; i<memory.length; i++) {
+            if(i % 10 == 0)
+                System.out.printf("%n%02d ",i);
+
+            System.out.printf(" %+05d ",memory[i]);
+        }
+
     }
 
 }
